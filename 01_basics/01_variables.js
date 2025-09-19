79aec937a1950f9c96fb0d23a91ac8f896086323
SWARAJ-prog <swarajingale33@gmail.com>
2025-09-20 03:03:33 +0530

    var don

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const pi = 3.14;
+let radius = 5;
+var area = pi * radius * radius;
+
+console.log("Area of circle:", area);
+
+const name = "swaraj";
+let email = "si@.com";
+var emailPass = "21212";
+city = "ngp";
+
+console.table([name , email , emailPass , city]) //prints a no. of data in tabular form 
+ 
