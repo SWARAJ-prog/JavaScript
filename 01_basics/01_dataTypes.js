8ee7f595f1f7250170c709dcab6e4b553bd5305e
SWARAJ-prog <swarajingale33@gmail.com>
2025-09-20 15:38:19 +0530

    done

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treats all js code as newer or latest version
+
+//alert(3+3) // shows a popup with the value but we don't use it as much and 
+            //also it's syntax has been changed in latest version
+            //as we are using node js not a browser
+
+console.log(3+3) ; console.log("swaraj") ;
+
+let name = "swaraj"; // string data type
+let age = 21; // number data type
+let isApproved = false; // boolean data type
+let firstName ;// undefined data type mtlb value abhi define nhi ki hai
+let selectedColor = null; // null data type it is a standalone value
+let Name = Symbol("swaraj");
+
+console.log(typeof Name);
--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,8 +6,7 @@ console.log("Area of circle:", area);
 
 const name = "swaraj";
 let email = "si@.com";
-var emailPass = "21212";
+var emailPass = "21212";   // prefer not to use var as much
 city = "ngp";
 
-console.table([name , email , emailPass , city]) //prints a no. of data in tabular form 
- 
+console.table([name , email , emailPass , city]) //prints a no. of data in tabular form
