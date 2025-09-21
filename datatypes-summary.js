037560c1003e8a2f9909dc9b9c80076c6e6d2e1d
SWARAJ-prog <swarajingale33@gmail.com>
2025-09-22 00:21:13 +0530

    done

---
+++ datatypes-summary.js
@@ -0,0 +1,21 @@
+// Primitive datatypes
+//it has 7 types: string , number , boolean , null , undefined , symbol , bigint
+
+// Reference datatypes(Non primitive datatypes)
+//it has 3 types : arrays , objects , functions
+// const heroes = ["shaktiman", "naagraj", "doga"];
+// let myObj = {name : "swaraj",
+//     age : 20,
+//     email : "swaraj@123"
+//     }
+// console.log(typeof myObj , myObj);
+// console.log(typeof heroes , heroes);
+
+//javaScript is a dynamically typed language 
+
+// Functions
+ const myFunction = function(){
+    console.log("this is my function");
+ }
+ console.log(typeof myFunction);
+ myFunction();
