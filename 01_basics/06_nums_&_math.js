403f374fa97e59e1283dac22f40cb0219349abec
SWARAJ-prog <swarajingale33@gmail.com>
2025-09-24 02:29:23 +0530

    done

---
+++ 01_basics/06_nums_&_math.js
@@ -0,0 +1,39 @@
+//********************* NUM **********************/
+// const score = 100;
+// // console.log(score);//it give directly stored value
+
+// const balance = new Number(10000);
+// console.log(typeof balance , balance);//it gives specific object type as number in this case
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toString());
+// // console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// // console.log(balance.toFixed(3));
+// const balance1 = 1234.7892;
+// console.log(balance1.toPrecision(4));
+
+// const otherNumber = 1100000;
+// console.log(otherNumber.toLocaleString("en-IN"));//that gives comas to understand big numbers
+
+//********************* MATHS **********************/
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(-7.7));//it gives absolute value like +ve remains +ve but -ve converted to +ve
+// console.log(Math.round(7.7));//it rounds the value to nearest integer
+// console.log(Math.ceil(7.4));
+// console.log(Math.floor(7.7));
+// console.log(Math.min(1,2,3,4,5,-10,-100));//it gives minimum value
+// console.log(Math.max(1,2,3,4,5,-10,-100));
+// console.log(Math.random());//it gives random value between 0 to 1
+// console.log(Math.round(Math.random()*100));//it gives random value between 0 to 100
+// console.log(Math.round(Math.random()*10));//it gives random value between 0 to 10
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+console.log(Math.floor(Math.random()*(max-min+1))+min);
